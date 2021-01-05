58ffa59817088367c274f81e8a5ef7c5ae9e159f
sakilahmmad71 <sakilahmmad71@gmail.com>
2021-01-05 10:20:26 +0600

    arrays type annotation

---
+++ arrays/arrays.ts
@@ -0,0 +1,12 @@
+const cars: string[] = ['toyota', 'marcedes'];
+const dates: Date[] = [new Date(), new Date()];
+
+// 2D array
+const carByMake: string[][] = [['camero'], ['toyolo'], ['corola']];
+let bigArray: number[][];
+
+cars.map((car): string => {
+  return car;
+})
+
+const greatTImes: (string | Date)[] = [new Date(), 'saturday'];
